04fa5970593453d1e4d449a0984b546d98a1a735
abkarada <abdurrahmankaradag19@gmail.com>
2025-10-08 15:06:00 +0300

    fix

--- simple-client.sh
+++ simple-client.sh
@@ -11,4 +11,4 @@ FILE=${3:-test-files/test_10mb.bin}
 
 echo "🔥 Starting Simple QUIC Client to $HOST:$PORT"
 echo "📁 File to transfer: $FILE"
-java -cp build/libs/QUICC-1.0-SNAPSHOT.jar quic.SimpleQuicFileTransfer client "$HOST" "$PORT" "$FILE"
+java -cp build/libs/quic-transfer.jar quic.SimpleQuicFileTransfer client "$HOST" "$PORT" "$FILE"
--- simple-server.sh
+++ simple-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PORT=${1:-9999}
 echo "🚀 Starting Simple QUIC Server on port $PORT..."
-java -cp build/libs/QUICC-1.0-SNAPSHOT.jar quic.SimpleQuicFileTransfer server "$PORT"
+java -cp build/libs/quic-transfer.jar quic.SimpleQuicFileTransfer server "$PORT"
